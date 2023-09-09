6b830ee4bd8cfc8429aa153deaec9887c65c2701
이지혜 <jihye@ijihyeui-MacBookAir.local>
2023-09-10 06:23:20 +0900

    Modify : Sub 페이지 UI 완성, moakData 연결

---
+++ src/components/Categories/SubCard.jsx
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { VscHeart, VscHeartFilled } from 'react-icons/vsc';
+
+const Card = () => {
+  const [subCardData, setSubCardData] = useState([]);
+
+  const isToggled = () => {
+    axios.post('/data/mainCard.json').then(() => {
+      axios.get('/data/mainCard.json').then(response => {
+        setSubCardData(response.data);
+      });
+    });
+  };
+
+  useEffect(() => {
+    axios.get('/data/mainCard.json').then(response => {
+      setSubCardData(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      {subCardData.map(data => (
+        <div
+          className="card relative shrink-0 w-[22.5%] rounded-xl shadow-md cursor-pointer"
+          key={data.id}
+        >
+          <div
+            className="showImg w-full h-52 bg-cover rounded-t-xl"
+            style={{ background: `url(${data.url})` }}
+          />
+          <div className="cardTop absolute top-0 p-2 flex justify-between w-full">
+            <div className="tag bg-brand">마감 임박</div>
+            <div onClick={isToggled}>
+              {data.isLiked ? (
+                <VscHeart className="w-6 h-6 cursor-pointer" />
+              ) : (
+                <VscHeartFilled className="w-6 h-6 text-brand cursor-pointer" />
+              )}
+            </div>
+          </div>
+          <div className="showReport p-2 pl-3">
+            <div>{data.name}</div>
+            <div>{data.place}</div>
+            <div>{data.period} </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+export default Card;
